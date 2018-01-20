113265279bea2c4004881f9e1d147f669ca503f1
SteveJones <benkong_ah@foxmail.com>
2018-01-20 12:38:26 +0800

    Commit Project

---
+++ ThreadDemo_OC/ThreadDemo_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThreadDemo_OC
+//
+//  Created by 江奔 on 2018/1/20.
+//  Copyright © 2018年 A8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ThreadDemo_OC/ThreadDemo_OCTests/ThreadDemo_OCTests.m
@@ -0,0 +1,39 @@
+//
+//  ThreadDemo_OCTests.m
+//  ThreadDemo_OCTests
+//
+//  Created by 江奔 on 2018/1/20.
+//  Copyright © 2018年 A8. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ThreadDemo_OCTests : XCTestCase
+
+@end
+
+@implementation ThreadDemo_OCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
